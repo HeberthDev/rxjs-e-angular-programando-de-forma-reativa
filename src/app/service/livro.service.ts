74564e4c0406e62e319fa96536a0fbd3a2bdc9a8
HeberthDev <heberthdeveloper@gmail.com>
2025-02-25 07:39:22 -0300

    Lidando com a resposta da API

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LivrosResultado } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class LivroService {
     private http: HttpClient,
   ) { }
 
-  buscar(valorDigitado: string): Observable<any> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
 
     return this.http.get(this.API, { params: params });
