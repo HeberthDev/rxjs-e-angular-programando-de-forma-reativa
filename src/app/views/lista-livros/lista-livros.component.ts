77fcbc1340b65d17e3e6b79c7943c74cb525506a
HeberthDev <heberthdeveloper@gmail.com>
2025-03-12 08:14:07 -0300

    Mostrando total de itens encontrados

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { catchError, debounceTime, EMPTY, filter, map, switchMap, tap, throwError } from 'rxjs';
-import { Item } from 'src/app/models/interfaces';
+import { catchError, debounceTime, filter, map, of, switchMap, tap, throwError } from 'rxjs';
+import { Item, LivrosResultado } from 'src/app/models/interfaces';
 import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
@@ -13,12 +13,26 @@ import { LivroService } from 'src/app/service/livro.service';
 export class ListaLivrosComponent {
   public campoBusca = new FormControl();
   public mensagemErro = '';
+  public livrosResultado: LivrosResultado;
 
   private pausa = 300;
 
   constructor(
     private livroService: LivroService,
   ) { }
+
+  totalDeLivros$ = this.campoBusca.valueChanges
+  .pipe(
+    debounceTime(this.pausa),
+    filter((valorDigitado) => valorDigitado.length >= 3),
+    tap(() => console.log('Fluxo Inicial')),
+    switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+    map(resultado => this.livrosResultado = resultado),
+    catchError(erro => {
+      console.log(erro);
+      return of();
+    })
+  );
 
   livrosEncontrados$ = this.campoBusca.valueChanges
     .pipe(
@@ -27,12 +41,14 @@ export class ListaLivrosComponent {
       tap(() => console.log('Fluxo Inicial')),
       switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
       tap((retornoAPI) => console.log(retornoAPI)),
+      map(resultado => resultado.items ?? []),
       map((items) => this.livrosResultadoParaLivros(items)),
       catchError(erro => {
-        this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.';
-        return EMPTY;
-      }),
-    )
+        // this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.';
+        console.log(erro);
+        return throwError(() => new Error(this.mensagemErro = 'Ops, ocorreu um erro. Recarregue a aplicação.'));
+      })
+    );
 
   public livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
     return items.map(item => {
